dc1360c09ca5c48f7a000b8b59f6c68fdb0c0017
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2021-11-21 06:52:41 +0100

    Added new feature to AuxiliaryMethods: convertStringToInt

--- AuxiliaryMethods.h
+++ AuxiliaryMethods.h
@@ -2,12 +2,14 @@
 #define AUXILIARYMETHODS_H
 
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 class AuxiliaryMethods {
 public:
     static string loadLine();
+    static int convertStringToInt(string number);
 };
 
 #endif // AUXILIARYMETHODS_H
